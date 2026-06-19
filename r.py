eebf33269d0bf1fb66cff6d4b627d420f139d04c
Maxim.R <sl0nh0ttabuch@gmail.com>
2026-06-19 12:06:47 +0300

    cleaning trash

--- r.py
+++
@@ -1,9 +0,0 @@
-import rpy2.robjects as ro
-from rpy2.robjects.packages import importr
-
-fedstat = importr("fedstatAPIr")
-
-data = fedstat.fedstat_data_load_with_filters(
-    indicator_id="31074",
-    filters=ro.ListVector({"Год": "2023"})
-)
